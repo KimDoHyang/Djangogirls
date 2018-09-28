be451081ec294f69f09b3d74f0227aea59207294
hanoul <hanoul1124@gmail.com>
2018-09-28 16:56:06 +0900

    post_list title을 a태그(post id값)을 통해 post_detail과연결.

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,23 @@ from django.template import loader
 from django.shortcuts import render
 # Create your views here.
 from django.utils import timezone
+import re
 
+
+# def post_detail(request):
+#     m = re.search(r'/posts/(?P<pk>\d+)/$', request.path)
+#     pk = m.group('pk')
+def post_detail(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {
+        'post': post,
+    }
+    # return HttpResponse(post.title)
+    return render(
+        request=request,
+        template_name='blog/post_detail.html',
+        context=context,
+    )
 
 
 def post_list(request):
@@ -43,7 +59,8 @@ def post_list(request):
     # content = open(templates_path, 'rt').read()
     """
     
-    :param request: 실제 HTTP요청에 대한 정보를 가진 객체
+    :param request: 실제 HTTP요청에 대한 정보를 가진 객체. urls.py(urlreslover에 등록된 패턴에 들어온 게 일치할 경우
+    여기에서 받아 실행한다.
     :return:
     """
     # 현재 지역에 맞는 날짜&시간 객체 할당
@@ -72,4 +89,112 @@ def post_list(request):
         template_name='blog/post_list.html',
         context=context,
     )
+
+
+# 1. 사용자가 브라우저의 주소표시줄을 사용해서(또는 하이퍼링크) 특정 주소에 요청
+# 2. 요청을 받은 서버는 해당 요청을 "어디선가" 동적으로 처리
+# 3. "어디선가" 처리된 결과를 서버가 사용자에게 반환
+# 4. 사용자는 요청한 결과를 브라우저에서 보게됨
+#
+#
+# ~/projects/
+#     django/
+#         djangogirls-tutorial/
+#             git설정 <- git remote add 주소를 git@ 으로 시작하는 주소로 추가
+#             .gitignore 설정
+#                 git, linux, macos, python, pycharm+all, jupyternotebook, django
+#
+#             pyenv virtualenv 3.6.6 fc-djangogirls
+#             pyenv local fc-djangogirls
+#
+# PyCharm설치 (Pro, Community무관) -> djangogirls-tutorial/ 폴더를 열어주기\
+# pycharm압축 푼 폴더로 이동 -> bin/폴더 이동 -> ./pycharm.sh로 실행
+#
+# ------------------------------
+#
+# PyCharm Interpreter
+#
+# Ubuntu
+#     ~/.pyenv/versions/<가상환경명>/bin/python
+# macOS
+#     /usr/local/var/pyenv/versions/<가상환경명>/bin/python
+#
+# ----------------------------------
+#
+# 평문 -(특정키로 암호화)->   암호문 전송   -> (특정키로 복호화)-평문
+#
+# 개인키: id_rsa
+# 공개키: id_rsa.pub
+#
+# 평문 -(개인키로 암호화)->  암호문 전송  -> (공개키로 복호화)-평문
+#
+#
+# Bob / Alice
+#
+# Bob의 개인키
+# Bob의 공개키
+#
+# Alice의 개인키
+# Alice의 공개키
+#
+# --------------------------
+#
+# Bob이 Alice에게 통신요청
+#
+# 1. Bob이 Alice에게 공개키 요청
+# 2. Bob이 평문을 Alice의 공개키로 암호화해서 전송
+# 3. Alice는 Bob이 보낸 암호문을 자신의 개인키로 복호화
+#
+#
+# -------------------------
+#
+# git 저장소에 명령을 보내기 전에, 특정 값을 자신의 개인키로 암호화
+# GitHub서버에서는 받은 요청을 내가 올려놓은 공개키로 복호화 시도
+# 성공한다면 -> 공개키를 올린 사람 본인을 입증할 수 있음
+# username/password 물어보지 않고 명령 실행
+#
+# -------------------------
+#
+# http://(어떤도메인)/posts/
+#     -> 블로그의 모든 Post목록
+#
+# http://(어떤도메인)/posts/1/
+#     -> 1번 Post를 보여줌
+#
+#
+# --------------------------
+#
+# Django project structure
+#
+# djangogirls-tutorial/ <-    프로젝트 폴더
+#     app/                <-    Django폴더
+#         config/            <-    Django설정 패키지
+#             settings.py    <-    Django설정 모듈
+#         manage.py        <-    Django유틸리티 모듈
+#     .git
+#     .gitignore
+#     .python-version
+#
+#
+# -------------------------
+#
+# MVC모델                Django
+#
+# Model        <->        Model (models.py)
+# View        <->        Template
+# Controller    <->        View (views.py)
+#
+# -------------------------
+#
+# runserver (웹 서버)
+# Django application
+#     urlresolver (config.urls)
+#
+#
+# 127.0.0.1:8000 -> runserver -> urlresolver
+#     -> r'^$'와 매치
+#     -> 해당 요청을 blog.views.post_list에 보냄
+#     -> 함수에서는 받은 요청을 처리한 결과를 리턴
+#     -> 리턴된 결과는 브라우저에 표시
+
 
--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,13 @@ from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.post_list)
+    url(r'^$', views.post_list),
+    # url(r'^posts/\d+/$', views.post_detail),
+    url(r'^posts/(?P<pk>\d+)/$', views.post_detail)
+    #위와 같은 경우, 그룹화를 시키게 되면 이 그룹화 한 것이 두번재 인수로서 전달되도록 한다.
+    # 이 경우 request가 오면 post_detail(request, pk=<요청된 값>)으로 전달된다.
+
+    # /posts/1/, /posts/777/, ...  <- 이 경우에 해당하는 정규표현식 패턴(/로 끝나야함)이
+    # blog.views.post_detail 여기로 전달되도록 설정 및 뷰 함수 구현.
+    # -> 이 뷰는 Post Detail이라는 문자열을 HttpResponse를 사용해 리턴
 ]
