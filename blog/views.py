c572f95568a0c3a3a304df6021191a8c9f0f0742
hanoul <hanoul1124@gmail.com>
2018-09-28 11:25:40 +0900

    post_list view에서 template를 loader로 가져와 render한 결괄르 리턴

--- blog/views.py
+++ blog/views.py
@@ -2,19 +2,25 @@ import os
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 from django.utils import timezone
 
 
 def post_list(request):
-    views_file_path = os.path.abspath(__file__)
-    blog_app_path = os.path.dirname(views_file_path)
-    app_dir = os.path.dirname(blog_app_path)
-    templates_path = os.path.join(app_dir, 'templates', 'blog', 'post_list.html')
+    template = loader.get_template('blog/post_list.html')
+    context = {}
+    content = template.render(context, request)
 
-    with open(templates_path, 'rt') as f:
-        content = f.read()
+
+    # views_file_path = os.path.abspath(__file__)
+    # blog_app_path = os.path.dirname(views_file_path)
+    # app_dir = os.path.dirname(blog_app_path)
+    # templates_path = os.path.join(app_dir, 'templates', 'blog', 'post_list.html')
+    #
+    # with open(templates_path, 'rt') as f:
+    #     content = f.read()
 
     # #or 파일객체 직접 사용 후 close
     # f = open(templates_path, 'rt')
@@ -29,7 +35,7 @@ def post_list(request):
     :return:
     """
     # 현재 지역에 맞는 날짜&시간 객체 할당
-    current_time = timezone.now()
+    # current_time = timezone.now()
     return HttpResponse(content
         # '<html>'
         # '<body>'
